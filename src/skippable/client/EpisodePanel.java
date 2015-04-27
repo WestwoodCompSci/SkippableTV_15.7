bfda616e6fdd87f9a03fbe015c1025aae0cf5ad2
Caysie98 <caysie98@gmail.com>
2015-04-27 12:53:39 -0500

    Change name to EpisodePanel +update

---
+++ src/skippable/client/EpisodePanel.java
@@ -0,0 +1,24 @@
+package skippable.client;
+
+import javax.swing.*;
+
+public class EpisodePanel extends JPanel
+{
+	
+	
+	public EpisodePanel()
+	{
+		
+		//TV show info
+		
+		
+		
+		//episode name/season/number
+		
+		//episode rating/skippability 
+		
+		//editable episode summary
+		
+		//comments
+	}
+}
