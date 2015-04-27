7e3d41bc70f5ee6ff70b64e96f0f2bd9b57b4daf
Caysie98 <caysie98@gmail.com>
2015-04-27 13:37:51 -0500

    added to episodepanel

--- src/skippable/client/EpisodePanel.java
+++ src/skippable/client/EpisodePanel.java
@@ -4,14 +4,17 @@ import javax.swing.*;
 
 public class EpisodePanel extends JPanel
 {
-	
+	Episode myEpisode;
 	
 	public EpisodePanel()
 	{
+		Box mainBox = Box.createVerticalBox();
 		
 		//TV show info
 		
-		
+		JTextField titleField = new JTextField(myEpisode.getTitle());
+		Box titleHBox = Box.createHorizontalBox();
+		Image skipBars = new Image("")										//Image??
 		
 		//episode name/season/number
 		
