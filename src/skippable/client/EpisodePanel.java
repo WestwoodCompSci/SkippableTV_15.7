1c93d3d0196aa2212ed6ea99fb6a3c12dae13831
Caysie98 <caysie98@gmail.com>
2015-04-29 13:20:18 -0500

    Update episodepanel

--- src/skippable/client/EpisodePanel.java
+++ src/skippable/client/EpisodePanel.java
@@ -1,27 +1,42 @@
 package skippable.client;
+import java.awt.Image;
+import java.util.List;
 
 import javax.swing.*;
 
 public class EpisodePanel extends JPanel
 {
 	Episode myEpisode;
+	//List<String> myComments;
 	
 	public EpisodePanel()
 	{
 		Box mainBox = Box.createVerticalBox();
 		
-		//TV show info
-		
+		//TV show info		
 		JTextField titleField = new JTextField(myEpisode.getTitle());
-		Box titleHBox = Box.createHorizontalBox();
-		Image skipBars = new Image("")										//Image??
+		Box titleHBox = Box.createHorizontalBox();							//contains bars, show name, show rating, season #, episode #
+		Image skipBars = new Image("");										//skip bars, unique to episode
+		Box showStarsV = Box.createVerticalBox();							//show name, show rating, season #, episode #
+		Box showStarsH = Box.createHorizontalBox();							//show name, show rating (image?)
+		JTextField showName = new JTextField(myEpisode.getShow().getTitle());
+		showStarsH.add(showName);
 		
-		//episode name/season/number
+		showStarsV.add(showStarsH);
 		
-		//episode rating/skippability 
+		//episode info
+		Box summTitle = Box.createHorizontalBox();
+		JButton editSumm = new JButton("Edit");
+		JButton pubSumm = new JButton("Publish");							//hidden until edit pressed
+		Box summaryBox = Box.createHorizontalBox();
 		
-		//editable episode summary
+		Box commentsH = Box.createHorizontalBox();							//strut
+		Box commentsV = Box.createVerticalBox();
 		
-		//comments
+		//strut
+		JButton addCom = new JButton("Add comment");
+		
+		
+		
 	}
 }
