1156e1add9ba8c1de39fcea6af6fac80be86cf88
lekro <meesayousa@gmail.com>
2015-04-29 13:34:53 -0500

    Initial episode and show classes.

---
+++ src/skippable/client/backend/Show.java
@@ -0,0 +1,29 @@
+package skippable.client.backend;
+
+import java.util.List;
+
+public class Show {
+
+	private String summary;
+	private int rating;
+	private String genre;
+	
+	private List<Episode> episodes;
+	
+	public String getShowSummary() {
+		return summary;
+	}
+	
+	public void setSummary(String summary) {
+		this.summary = summary;
+	}
+	
+	public void rateShow(int r) {
+		rating = r;
+	}
+	
+	public int getShowRating() {
+		return rating;
+	}
+	
+}
