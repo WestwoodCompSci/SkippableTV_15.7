305108e52073e423203db9c45701457e29421f26
trevercol <trevercol@gmail.com>
2015-05-01 13:46:55 -0500

    Register Panel

--- src/skippable/client/gui/LoginPanel.java
+++ src/skippable/client/gui/LoginPanel.java
@@ -26,38 +26,84 @@ public class LoginPanel extends JPanel
 	{
 		myLoginPage = new JPanel();
 		
-		final JFrame testframe = new JFrame("Testing the login panel");
+		final JFrame testframe = new JFrame("Testing the panels.");
 		testframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 		testframe.setPreferredSize(new Dimension(600,400));
 		testframe.setLocation(350, 250);
 		
-		final JPanel panel = new JPanel();
-		panel.setPreferredSize(new Dimension(600,400));
-		panel.setLocation(600, 400);
+	//Login Panel
+		
+		final JPanel lpanel = new JPanel();
+		lpanel.setPreferredSize(new Dimension(600,400));
+		lpanel.setLocation(600, 400);
 		
 		Box lBox = Box.createVerticalBox();
-		panel.add(lBox);
-		testframe.add(panel);
+		lpanel.add(lBox);
+		//testframe.add(lpanel);
 		
-		Box registerBox = Box.createHorizontalBox();
-		Box usernameBox = Box.createHorizontalBox();
-		Box passwordBox = Box.createHorizontalBox();
+		Box registerlBox = Box.createHorizontalBox();
+		Box usernamelBox = Box.createHorizontalBox();
+		Box passwordlBox = Box.createHorizontalBox();
 		
-		lBox.add(registerBox);
+		lBox.add(registerlBox);
 		lBox.add(Box.createVerticalStrut(50));
-		lBox.add(usernameBox);
+		lBox.add(usernamelBox);
 		lBox.add(Box.createVerticalStrut(50));
-		lBox.add(passwordBox);
+		lBox.add(passwordlBox);
 		lBox.add(Box.createVerticalStrut(50));
 		
-		//Testing
+		final JTextField usernameFieldL = new JTextField();
+		final JTextField passwordFieldL = new JTextField();
+		final JButton forgotPasswordBL = new JButton("Forgot Password");
+		final JButton signInBL = new JButton("Sign In");
+		final JButton registerBL = new JButton("Register");
+		
+		usernameFieldL.setNextFocusableComponent(passwordFieldL);	
+		signInBL.setNextFocusableComponent(forgotPasswordBL);
+		forgotPasswordBL.setNextFocusableComponent(registerBL);
+		
+		//
+		
+	// Register Panel
+
+		final JPanel rpanel = new JPanel();
+		rpanel.setPreferredSize(new Dimension(600,400));
+		rpanel.setLocation(600, 400);
+		
+		Box rBox = Box.createVerticalBox();
+		rpanel.add(rBox);
+		testframe.add(rpanel);
+		
+		Box usernamerBox = Box.createHorizontalBox();
+		Box passwordrBox = Box.createHorizontalBox();
+		Box emailrBox = Box.createHorizontalBox();
+		Box registerrBox = Box.createHorizontalBox();
+		
+		rBox.add(usernamerBox);
+		rBox.add(Box.createVerticalStrut(50));
+		rBox.add(passwordrBox);
+		rBox.add(Box.createVerticalStrut(50));
+		rBox.add(emailrBox);
+		rBox.add(Box.createVerticalStrut(50));
+		rBox.add(registerrBox);
+		
+		final JTextField usernameFieldR = new JTextField();
+		final JTextField passwordFieldR = new JTextField();
+		final JTextField emailFieldR = new JTextField();
+		final JButton registerBR = new JButton("Register");
+		//
+		
+	//Testing
 		//panel.setBackground(Color.RED);
+	
+//------------------------------------------------------------------------------		
+		
+	//Login Panel	
 		
 		//Register Box
-		final JButton registerB = new JButton("Register");
 		
-		registerB.addActionListener(new ActionListener() {
+		registerBL.addActionListener(new ActionListener() {
 
 			@Override
 			public void actionPerformed(ActionEvent e) 
@@ -65,19 +111,17 @@ public class LoginPanel extends JPanel
 								
 			}} );
 		
-		registerBox.add(Box.createHorizontalStrut(500));
-		registerBox.add(registerB);
+		registerlBox.add(Box.createHorizontalStrut(500));
+		registerlBox.add(registerBL);
 		
 		//Password Box
-		final JTextField passwordField = new JTextField();
-		final JButton signInB = new JButton("Sign In");
 		
-		passwordField.addKeyListener(new KeyListener() {
+		passwordFieldL.addKeyListener(new KeyListener() {
 			@Override
 			public void keyPressed(KeyEvent arg0) {
 				if (arg0.getKeyCode() == KeyEvent.VK_ENTER)
 				{
-					signInB.doClick();
+					signInBL.doClick();
 				}
 			}
 
@@ -93,7 +137,7 @@ public class LoginPanel extends JPanel
 				
 			}} );
 		
-		signInB.addActionListener(new ActionListener() {
+		signInBL.addActionListener(new ActionListener() {
 
 			@Override
 			public void actionPerformed(ActionEvent e) 
@@ -101,49 +145,146 @@ public class LoginPanel extends JPanel
 								
 			}} );
 		
-		passwordBox.add(Box.createHorizontalStrut(100));
-		passwordBox.add(passwordField);
-		passwordBox.add(Box.createHorizontalStrut(50));
-		passwordBox.add(signInB);
-		passwordBox.add(Box.createHorizontalStrut(130));
+		passwordlBox.add(Box.createHorizontalStrut(100));
+		passwordlBox.add(passwordFieldL);
+		passwordlBox.add(Box.createHorizontalStrut(50));
+		passwordlBox.add(signInBL);
+		passwordlBox.add(Box.createHorizontalStrut(130));
 		
 		//Username Box
-		final JTextField usernameField = new JTextField();
-		final JButton forgotPasswordB = new JButton("Forgot Password");
 		
-		usernameField.addKeyListener(new KeyListener() {
+		usernameFieldL.addKeyListener(new KeyListener() {
 				@Override
+			public void keyPressed(KeyEvent arg0) {
+				//JOptionPane.showMessageDialog(frame, arg0.getKeyCode());
+					System.out.println("hi");
+				if (arg0.getKeyCode() == KeyEvent.VK_ENTER)
+				{
+					signInBL.doClick();
+				}
+				if (arg0.getKeyCode() == KeyEvent.VK_TAB)
+				{
+					passwordFieldL.grabFocus();
+				}
+			}
+				@Override
+			public void keyReleased(KeyEvent arg0) {
+				// TODO Auto-generated method stub
+				
+			}
+				@Override
+			public void keyTyped(KeyEvent arg0) {
+				// TODO Auto-generated method stub
+				
+			}} );
+		
+		forgotPasswordBL.addActionListener(new ActionListener() {
+				@Override
+			public void actionPerformed(ActionEvent e) 
+			{
+							
+			}} );
+		
+		usernamelBox.add(Box.createHorizontalStrut(100));
+		usernamelBox.add(usernameFieldL);
+		usernamelBox.add(Box.createHorizontalStrut(20));
+		usernamelBox.add(forgotPasswordBL);
+		usernamelBox.add(Box.createHorizontalStrut(100));
+	
+//---------------------------------------------------------------------------------------	
+
+	//Register Panel	
+		
+		//Username Box
+		
+		usernameFieldR.addKeyListener(new KeyListener() {
+			@Override
+			public void keyPressed(KeyEvent arg0) {
+				//JOptionPane.showMessageDialog(frame, arg0.getKeyCode());
+					if (arg0.getKeyCode() == KeyEvent.VK_ENTER)
+					{
+						registerBR.doClick();
+					}
+			}
+				@Override
+			public void keyReleased(KeyEvent arg0) {
+					// TODO Auto-generated method stub
+			
+			}
+			@Override
+			public void keyTyped(KeyEvent arg0) {
+				// TODO Auto-generated method stub
+			
+			}} );
+	
+		usernamerBox.add(Box.createHorizontalStrut(0));
+		usernamerBox.add(usernameFieldR);
+		usernamerBox.add(Box.createHorizontalStrut(0));
+		
+		//Password Box
+	
+		passwordFieldR.addKeyListener(new KeyListener() {
+			@Override
 			public void keyPressed(KeyEvent arg0) {
 				//JOptionPane.showMessageDialog(frame, arg0.getKeyCode());
 				if (arg0.getKeyCode() == KeyEvent.VK_ENTER)
 				{
-					signInB.doClick();
+					registerBR.doClick();
 				}
 			}
-				@Override
+			@Override
 			public void keyReleased(KeyEvent arg0) {
 				// TODO Auto-generated method stub
-				
+		
 			}
-				@Override
+			@Override
 			public void keyTyped(KeyEvent arg0) {
 				// TODO Auto-generated method stub
-				
-			}} );
 		
-		forgotPasswordB.addActionListener(new ActionListener() {
-				@Override
+			}} );
+
+		passwordrBox.add(Box.createHorizontalStrut(0));
+		passwordrBox.add(passwordFieldR);
+		
+		//Email Box
+		
+		emailFieldR.addKeyListener(new KeyListener() {
+			@Override
+			public void keyPressed(KeyEvent arg0) {
+				//JOptionPane.showMessageDialog(frame, arg0.getKeyCode());
+				if (arg0.getKeyCode() == KeyEvent.VK_ENTER)
+				{
+					registerBR.doClick();
+				}
+			}
+			@Override
+			public void keyReleased(KeyEvent arg0) {
+				// TODO Auto-generated method stub
+		
+			}
+			@Override
+			public void keyTyped(KeyEvent arg0) {
+				// TODO Auto-generated method stub
+		
+			}} );
+
+		emailrBox.add(Box.createHorizontalStrut(0));
+		emailrBox.add(emailFieldR);
+			
+		//Register Box
+	
+		registerBR.addActionListener(new ActionListener() {
+
+			@Override
 			public void actionPerformed(ActionEvent e) 
 			{
 								
 			}} );
 		
-		usernameBox.add(Box.createHorizontalStrut(100));
-		usernameBox.add(usernameField);
-		usernameBox.add(Box.createHorizontalStrut(20));
-		usernameBox.add(forgotPasswordB);
-		usernameBox.add(Box.createHorizontalStrut(100));
-	
+		registerrBox.add(Box.createHorizontalStrut(0));
+		registerrBox.add(registerBR);
+		
+//---------------------------------------------------------------------------------------			
 		testframe.pack();
 		testframe.setVisible(true);
 		
