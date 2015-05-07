19e2269f1b20a20d5ca38ac020184c74670b0dd5
Caysie98 <caysie98@gmail.com>
2015-05-07 12:47:30 -0500

    update episodepanel

--- src/skippable/client/gui/EpisodePanel.java
+++ src/skippable/client/gui/EpisodePanel.java
@@ -13,11 +13,11 @@ import skippable.client.backend.Episode;
 public class EpisodePanel extends JPanel
 {
 	Episode myEpisode;
-
+	int mySkip;
 	
 	public EpisodePanel(Episode ep)
 	{
-		//mySkip = myEpisode.getEpisodeSkippability();
+		mySkip = myEpisode.getEpisodeSkippability();
 															//int --> String --> Image ?
 		myEpisode = ep;
 		
@@ -33,6 +33,51 @@ public class EpisodePanel extends JPanel
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}							//skip bars, unique to episode
+		
+		
+		Box skipBarsBox = Box.createVerticalBox();
+		
+		JButton bar1 = new JButton();
+		if (mySkip>=1) {
+			  try {
+			    Image img = ImageIO.read(getClass().getResource("blackRectangle.png"));
+			    bar1.setIcon(new ImageIcon(img));
+			  } catch (IOException ex) {
+			  }}
+		
+		JButton bar2 = new JButton();
+		if (mySkip>=2) {
+			  try {
+			    Image img = ImageIO.read(getClass().getResource("blackRectangle.png"));
+			    bar2.setIcon(new ImageIcon(img));
+			  } catch (IOException ex) {
+			  }}
+		
+		JButton bar3 = new JButton();
+		if (mySkip>=3) {
+			  try {
+			    Image img = ImageIO.read(getClass().getResource("blackRectangle.png"));
+			    bar3.setIcon(new ImageIcon(img));
+			  } catch (IOException ex) {
+			  }}
+		
+		JButton bar4 = new JButton();
+		if (mySkip==4) {
+			  try {
+			    Image img = ImageIO.read(getClass().getResource("blackRectangle.png"));
+			    bar4.setIcon(new ImageIcon(img));
+			  } catch (IOException ex) {
+			  }}
+
+		skipBarsBox.add(bar1);
+		skipBarsBox.add(Box.createHorizontalStrut(5));
+		skipBarsBox.add(bar2);
+		skipBarsBox.add(Box.createHorizontalStrut(5));
+		skipBarsBox.add(bar3);
+		skipBarsBox.add(Box.createHorizontalStrut(5));
+		skipBarsBox.add(bar4);
+		
+		
 		Box showStarsV = Box.createVerticalBox();							//show name, show rating, season #, episode #
 		Box showStarsH = Box.createHorizontalBox();							//show name, show rating (image?)
 		JTextField showName = new JTextField(myEpisode.getShow().getTitle());
@@ -40,28 +85,35 @@ public class EpisodePanel extends JPanel
 		showStarsH.add(new JTextField(myEpisode.getShow().getShowRating()));
 		showStarsV.add(showStarsH);
 		
-		Box pic = Box.createHorizontalBox();
-		pic.paint(skipBars.getGraphics());
-		titleHBox.add(pic);						 							//figure out how to add image to component
+		//Box pic = Box.createHorizontalBox();
+		//pic.paint(skipBars.getGraphics());
+		titleHBox.add(skipBarsBox);
 		
 		//episode info
 		Box summTitle = Box.createHorizontalBox();
 		JTextField summTitleWords = new JTextField("Episode Summary");
 		JButton editSumm = new JButton("Edit");
 		JButton pubSumm = new JButton("Publish");							//hidden until edit pressed
-		Box summaryBox = Box.createHorizontalBox();
+		Box summaryBox = Box.createVerticalBox();				//changed from horizontal
+		JTextArea summText = new JTextArea("");
+		summText.setEditable(false);
 		
 		Box commentsH = Box.createHorizontalBox();							//strut
 		Box commentsV = Box.createVerticalBox();
 		
 		summTitle.add(summTitleWords);
 		summTitle.add(editSumm);
-		pubSumm.setVisible(false);
+		pubSumm.setEnabled(false);
 		summTitle.add(pubSumm);
+		summaryBox.add(summTitle);
+		summaryBox.add(summText);
 		
 		
 		//strut
 		JButton addCom = new JButton("Add comment");
+		
+		
+		
 		
 		
 		
